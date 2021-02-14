3af6153862795127dd058848937f4ae7f9f28e8a
eddiebergman <eddiebergmanhs@gmail.com>
2021-02-14 19:34:58 +0100

    Updated the time buffers for selectors

--- tpotbench/benchmark.py
+++ tpotbench/benchmark.py
@@ -199,8 +199,15 @@ class Benchmark:
                     self.env.refresh_info()
                     info = self.env.info()
                     in_progress = info['pending'] + info['running']
+
+                    time_buffer = 0.5
+                    # Selector jobs need a long buffer
+                    if job.job_type() == 'selector':
+                        time_buffer = 1.0
+
                     if not job.name() in in_progress:
-                        self.env.run(job, slurm_job_options(job))
+                        self.env.run(job, slurm_job_options(job,
+                                                            buffer=time_buffer))
                 else:
                     print(f'running {job.name()}')
                     self.env.run(job, {})
